e6c1303262794b4a1b99f4a970ce47a90566c532
unknown <cindywijayab312@gmail.com>
2026-05-25 14:27:50 +0700

    update db multiple product, add orderController logic temporary stock lock

--- backend/database/lastbite.sql
+++ backend/database/lastbite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 19 Bulan Mei 2026 pada 18.37
+-- Waktu pembuatan: 25 Bulan Mei 2026 pada 09.23
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -55,9 +55,21 @@ CREATE TABLE `delivery` (
 CREATE TABLE `order` (
   `orderID` int(11) NOT NULL,
   `userID` int(11) NOT NULL,
-  `productID` int(11) NOT NULL,
   `status` varchar(50) DEFAULT 'pending',
   `total_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `orderItemID` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  `productID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -168,7 +180,14 @@ ALTER TABLE `delivery`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`orderID`),
-  ADD KEY `userID` (`userID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indeks untuk tabel `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`orderItemID`),
+  ADD KEY `orderID` (`orderID`),
   ADD KEY `productID` (`productID`);
 
 --
@@ -230,6 +249,12 @@ ALTER TABLE `order`
   MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `orderItemID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `product`
 --
 ALTER TABLE `product`
@@ -279,8 +304,14 @@ ALTER TABLE `delivery`
 -- Ketidakleluasaan untuk tabel `order`
 --
 ALTER TABLE `order`
-  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
-  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`);
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+--
+-- Ketidakleluasaan untuk tabel `order_item`
+--
+ALTER TABLE `order_item`
+  ADD CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `order` (`orderID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`) ON DELETE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `product`
